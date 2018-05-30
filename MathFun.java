fd8100240510d7546ef7c8391a683e615d308836
toma3233 <38714456+toma3233@users.noreply.github.com>
2018-05-30 00:01:37 -0700

    Create MathFun.java

---
+++ MathFun.java
@@ -0,0 +1,35 @@
+/** 
+ * 
+ * Tom Abraham
+ * Period #1
+ *
+ */
+public class MathFun {  
+
+	public static void main(String[] args) {
+		
+		calculate();
+		
+		}
+	
+	public static void calculate() {
+		int intAdd = 4 + 9;
+		System.out.println("4 + 9 = " + intAdd);
+		int intDivide = 46 / 7;
+		System.out.println("46 / 7 = " + intDivide);
+		int intModulus = 46 % 7;
+		System.out.println("46 % 7 = " + intModulus);
+		double intMultiply = 2 * 3.0;
+		System.out.println("2 * 3.0 = " + intMultiply);
+		double dDiv = (double)25 / 4;
+		System.out.println("(double)25 / 4 = " + dDiv);
+		int doubleToIntAdd = (int)7.75 + 2;
+		System.out.println("(int)7.75 + 2 = " + doubleToIntAdd);
+		char letter = 'P';
+		int number = 80;
+		System.out.println("(int)'P' = " + (int)letter);
+		char intToChar = (char)105;
+		System.out.println("(char)105 = " + intToChar);
+		
+	}
+	}
